6eb47c6a91c9d87efdd8213fc60dde84537d2237
GlenErrington <glenerrington@aol.com>
2021-11-18 14:46:48 +0000

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bas
+#!/usr/bin/env bash
 sudo cp autoAP.sh /usr/local/bin/autoAP.sh && sudo cp install-autoAP /usr/local/bin/install-autoAP && sudo cp rpi-networkconfig /usr/local/bin/rpi-networkconfig
 sudo chmod 755 /usr/local/bin/autoAP.sh /usr/local/bin/install-autoAP /usr/local/bin/rpi-networkconfig
 sudo /usr/local/bin/install-autoAP
